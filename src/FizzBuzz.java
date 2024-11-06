64fb8965359ca71def622fc18f558e6d0383b063
claGiaSW <claudia.giancola@softwire.com>
2024-11-06 16:43:46 +0000

    basic fizzbuzz using java ArrayList

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,17 +1,25 @@
+import java.util.ArrayList;
+
 public class FizzBuzz {
     public static void main(String[] args) throws Exception {
 
+        ArrayList<String> result = new ArrayList<String>();
+
         for (int i = 1; i <= 100; i++) {
 
-            if ( i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
+            if (i % 3 == 0) {
+                result.add("Fizz");
             }
+            if (i % 5 == 0) {
+                result.add("Buzz");
+            }
+            if (result.size() == 0) {
+                result.add(String.valueOf(i));
+            }
+
+            System.out.println(String.join("", result));
+
+            result.clear();
         }
     }
 }
