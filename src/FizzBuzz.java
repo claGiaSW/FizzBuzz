1c9da0eac16e82eee73096865463c3a9c5fc8df7
claGiaSW <claudia.giancola@softwire.com>
2024-11-06 17:47:23 +0000

    wip Stream<String>

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,6 +1,36 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class FizzBuzz {
+
+    public class FizzBuzzBangStream {
+
+        public static List<Integer> range = IntStream.range(1, 201).boxed().collect(Collectors.toList());
+
+        public static Stream<String> create() {
+            
+            Stream<String> resultStream = range.stream()
+            .map(int -> {
+                if (int % 3 == 0) {
+                    Integer.toString(int);
+                    int = "Fizz";
+                }
+                if (i % 5 == 0) {
+                    result.add("Buzz");
+                }
+                if (i % 7 == 0) {
+                    result.add("Bang");
+                }
+            });
+            
+            return resultStream;
+            
+        }
+    }
+
     public static void main(String[] args) throws Exception {
 
         ArrayList<String> result = new ArrayList<String>();
@@ -12,6 +42,9 @@ public class FizzBuzz {
             }
             if (i % 5 == 0) {
                 result.add("Buzz");
+            }
+            if (i % 7 == 0) {
+                result.add("Bang");
             }
             if (result.size() == 0) {
                 result.add(String.valueOf(i));
